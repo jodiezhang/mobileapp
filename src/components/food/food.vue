b682e73bb38ea964423ba0e6d9e395af29391b31
Judy Zhang <20080904@163.com>
2019-06-22 23:20:49 +0800

    商品详情页rating部分

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,133 +1,146 @@
 <template>
- <transition
+  <transition
     name="move"
     @after-leave="afterLeave"
   >
-<div class="food" v-show="visible">
-    <cube-scroll ref="scroll">
+    <div class="food" v-show="visible">
+      <cube-scroll ref="scroll">
         <div class="food-content">
-            <div class="image-header">
-                <img :src="food.image">
-                <div class="back" @click="hide">
-                    <i class="icon-arrow_lift"></i>
-                </div>    
-            </div> 
-            <div class="content">
-                <h1 class="title">{{food.name}}</h1>
-                <div class="detail">
-                    <span class="sell-count">月售{{food.sellCount}}份</span>
-                    <span class="rating">好评率{{food.rating}}%</span>
-                </div>
-                <div class="price">
-                    <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
-                </div> 
-                <div class="cart-control-wrapper">
-                    <cart-control @add="addFood" :food="food"></cart-control>
-                </div>
-               <transition name="fade">
+          <div class="image-header">
+            <img :src="food.image">
+            <div class="back" @click="hide">
+              <i class="icon-arrow_lift"></i>
+            </div>
+          </div>
+          <div class="content">
+            <h1 class="title">{{food.name}}</h1>
+            <div class="detail">
+              <span class="sell-count">月售{{food.sellCount}}份</span>
+              <span class="rating">好评率{{food.rating}}%</span>
+            </div>
+            <div class="price">
+              <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+            </div>
+            <div class="cart-control-wrapper">
+              <cart-control @add="addFood" :food="food"></cart-control>
+            </div>
+            <transition name="fade">
               <div @click="addFirst" class="buy" v-show="!food.count">
                 加入购物车
               </div>
-            </transition>             
-            </div>  
-             <split v-show="food.info"></split>
-            <div class="info" v-show="food.info">
-                <h1 class="title">商品信息</h1>
-                <p class="text">{{food.info}}</p>
+            </transition>
+          </div>
+          <split v-show="food.info"></split>
+          <div class="info" v-show="food.info">
+            <h1 class="title">商品信息</h1>
+            <p class="text">{{food.info}}</p>
+          </div>
+          <split></split>
+          <div class="rating">
+            <h1 class="title">商品评价</h1>
+            <rating-select
+              @select="onSelect"
+              @toggle="onToggle"
+              :selectType="selectType"
+              :onlyContent="onlyContent"
+              :desc="desc"
+              :ratings="ratings">
+            </rating-select>
+            <div class="rating-wrapper">
+              <ul v-show="computedRatings && computedRatings.length">
+                <li
+                  v-for="(rating,index) in computedRatings"
+                  class="rating-item border-bottom-1px"
+                  :key="index"
+                >
+                  <div class="user">
+                    <span class="name">{{rating.username}}</span>
+                    <img class="avatar" width="12" height="12" :src="rating.avatar">
+                  </div>
+                  <div class="time">{{format(rating.rateTime)}}</div>
+                  <p class="text">
+                    <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
+                  </p>
+                </li>
+              </ul>
+              <div class="no-rating" v-show="!computedRatings || !computedRatings.length">暂无评价</div>
             </div>
-            <split></split>
-            <div class="rating">
-                <h1 class="title">商品评价</h1>
-                <!-- <rating-select
-                   @select="onSelect"
-                   @toggle="onToggle"
-                   :selectType="selectType"
-                   :onlyContent="onlyContent"
-                   :desc="desc"
-                   :ratings="ratings">
-                </rating-select> -->
-                <!-- <div class="rating-wrapper">
-                    <ul v-show="computedRatings && computedRatings.length">
-                        <li
-                        v-for="(rating,index) in computedRatings"
-                        class="rating-item border-bottom-1px"
-                        :key="index">
-                        <div class="user">
-                            <span class="name">{{rating.username}}</span>
-                            <img class="avatar" width="12" height="12" :src="rating.avatar">
-                        </div>
-                        <div class="time">{{format(rating.rateTime)}}</div>
-                        <p class="text">
-                            <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>{{rating.text}}
-                        </p>        
-                        </li>    
-                    </ul>  
-                    <div class="no-rating" v-show="!computedRatings || computedRatings.length">暂无评价</div>  
-                </div>     -->
-            </div>          
-        </div>    
-    </cube-scroll>
-</div>
-</transition>
+          </div>
+        </div>
+      </cube-scroll>
+    </div>
+  </transition>
 </template>
-<script>
-  import CartControl from 'components/cart-control/cart-control'
-  import popupMixin from 'common/mixins/popup'
-  import Split from 'components/split/split'
 
-  const EVENT_SHOW='show'
-  const EVENT_LEAVE='leave'
-  const EVENT_ADD='add'
+<script type="text/ecmascript-6">
+  import moment from 'moment'
+  import CartControl from 'components/cart-control/cart-control'
+  import RatingSelect from 'components/rating-select/rating-select'
+  import Split from 'components/split/split'
+  import ratingMixin from 'common/mixins/rating'
+  import popupMixin from 'common/mixins/popup'
+
+  const EVENT_SHOW = 'show'
+  const EVENT_ADD = 'add'
+  const EVENT_LEAVE = 'leave'
+
   export default {
-    name:"food",
-    mixins: [popupMixin],
-    props:{
-        food:{
-            type:Object
-        }
+    name: 'food',
+    mixins: [ratingMixin, popupMixin],
+    props: {
+      food: {
+        type: Object
+      }
     },
-    data(){
-        return {
-            desc:{
-                all:'全部',
-                positive:'推荐',
-                negative:'吐槽'
-            }
+    data() {
+      return {
+        desc: {
+          all: '全部',
+          positive: '推荐',
+          negative: '吐槽'
         }
+      }
     },
-    created(){
-      this.$on(EVENT_SHOW,()=>{
-         this.$nextTick(()=>{this.$refs.scroll.refresh()})
+    computed: {
+      ratings() {
+        return this.food.ratings
+      }
+    },
+    created() {
+      this.$on(EVENT_SHOW, () => {
+        this.$nextTick(() => {
+          this.$refs.scroll.refresh()
+        })
       })
     },
-    methods:{
+    methods: {
+      afterLeave() {
+        this.$emit(EVENT_LEAVE)
+      },
       addFirst(event) {
-          this.$set(this.food,'count',1)
-          console.log(event.target)
-          this.$emit(EVENT_ADD,event.target)
-        
+        this.$set(this.food, 'count', 1)
+        this.$emit(EVENT_ADD, event.target)
       },
       addFood(target) {
-          this.$emit(EVENT_ADD,target)
+        this.$emit(EVENT_ADD, target)
       },
-      afterLeave(){
-          this.$emit(EVENT_LEAVE)
+      format(time) {
+        return moment(time).format('YYYY-MM-DD hh:mm')
       }
-
-      
-
-
     },
     components: {
       CartControl,
+      RatingSelect,
       Split
     }
-}
+  }
 </script>
+
 <style lang="stylus" scoped>
-@import "~common/stylus/variable"
-@import "~common/stylus/mixin.styl"
- .food
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin.styl"
+
+  .food
     position: fixed
     left: 0
     top: 0
